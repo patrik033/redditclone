3e85a3b5c39690617d65c5f115c75f7dbc0cdb59
Patrik Beijar Odh <patrik.odh@gmail.com>
2025-07-08 01:02:30 +0200

    Init

---
+++ src/app/(tabs)/inbox.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const InboxScreen = () => {
+    return (
+        <View>
+            <Text>Inbox Screen</Text>
+        </View>
+    )
+}
+
+export default InboxScreen;
