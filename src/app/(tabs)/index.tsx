edd64d00b2a5cfea532b90354b81b54cfe4ad0ba
Patrik Beijar Odh <patrik.odh@gmail.com>
2025-07-08 12:10:12 +0200

    HomeScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,57 +1,22 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View,FlatList } from "react-native";
+import PostListItem from "../../components/PostListItem";
 import posts from "../../../assets/data/posts.json";
-import { formatDistanceToNowStrict } from "date-fns";
 
 const HomeScreen = () => {
 
-    const post = posts[0];
     return (
-        <View style={{ paddingHorizontal: 15, paddingVertical: 10, flex: 1 }}>
+        <View style={{ flex: 1 }}>
+            <FlatList
+                data={posts}
+                renderItem={({ item }) => <PostListItem post={item} />}
+                keyExtractor={(item) => item.id}
+            />
             
-            {/* Post Header */}
-            <View style={{ flexDirection: 'row', gap: 10 }}>
-                <Image source={{ uri: post.group.image }} style={styles.image} />
-                <Text style={{ fontWeight: 'bold' }}>{post.group.name}</Text>
-                <Text style={{ fontSize: 12, color: 'gray' }}>{formatDistanceToNowStrict(new Date(post.created_at))}</Text>
-                <View style={{ marginLeft: 'auto' }}>
-                    <Text style={styles.joinButtonText}>Join</Text>
-                </View>
-            </View>
-
-            {/* Content */}
-            <Text style={styles.title}>{post.title}</Text>
-            <Image source={{ uri: post.image ?? undefined }} style={{ width: '100%', aspectRatio: 4 / 3, borderRadius: 15 }} />
-            <Text numberOfLines={4} style={{ fontSize: 14, color: 'gray' }}>{post.description}</Text>
-
-            {/* Footer */}
-            <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
-
-            </View>
-
         </View>
+
+
     )
 }
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({
-    image: {
-        height: 20,
-        aspectRatio: 1,
-        borderRadius: 10
-    },
-    joinButtonText: {
-        backgroundColor: '#0d469b',
-        color: 'white',
-        paddingVertical: 2,
-        paddingHorizontal: 7,
-        borderRadius: 10,
-        fontWeight: 'bold'
-    },
-    title: {
-        fontSize: 17,
-        fontWeight: 'bold',
-        letterSpacing: 0.5
-    }
-
-})
